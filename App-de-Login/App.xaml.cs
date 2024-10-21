cc6d77afe13fef14d933db59a12fc5f7d430d77e
Jvcn007 <joaovitorvcn07@gmail.com>
2024-10-21 20:40:39 -0300

    Add files in the project

--- App-de-Login/App.xaml.cs
+++ App-de-Login/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace App_de_Login
+﻿
+namespace App_de_Login
 {
     public partial class App : Application
     {
@@ -6,7 +7,16 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new Login();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+
         }
     }
 }
---
+++ App-de-Login/Protegida.xaml.cs
@@ -0,0 +1,9 @@
+namespace App_de_Login;
+
+public partial class Protegida : ContentPage
+{
+	public Protegida()
+	{
+		InitializeComponent();
+	}
+}
