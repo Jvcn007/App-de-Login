a6df42748c7afd6166fff13e28b30f999bec18d3
Jvcn007 <joaovitorvcn07@gmail.com>
2024-10-26 11:00:17 -0300

    New Version

---
+++ App-de-Login/DadosUsuario.cs
@@ -0,0 +1,9 @@
+﻿namespace App_de_Login
+{
+    public class DadosUsuario
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
--- App-de-Login/Login.xaml.cs
+++ App-de-Login/Login.xaml.cs
@@ -7,8 +7,50 @@ public partial class Login : ContentPage
 		InitializeComponent();
 	}
 
-    private void Button_Clicked(object sender, EventArgs e)
+    private async void Button_Clicked(object sender, EventArgs e)
     {
+		try
+		{
+			List<DadosUsuario> lista_usuarios = new List<DadosUsuario>()
+			{
+				new DadosUsuario()
+				{
+					Usuario = "jose",
+					Senha = "123"
+				},
+				new DadosUsuario()
+				{
+					Usuario = "maria",
+					Senha = "321"
+				}
+			};
 
+			DadosUsuario dados_digitados = new DadosUsuario()
+			{
+				Usuario = txt_usuario.Text,
+		/* Se n funcionar colocar string*/  Senha = txt_senha.Text
+			};
+
+
+			//LINQ
+			if (lista_usuarios.Any(
+
+				i => dados_digitados.Usuario == i.Usuario && dados_digitados.Senha == i.Senha))
+			{
+				await SecureStorage.Default.SetAsync("usuario_logado", dados_digitados.Usuario);
+
+				App.Current.MainPage = new Protegida();
+			}
+			else 
+			{
+				throw new Exception("Usuário ou senha incorreta!");
+			}
+
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+		}
+			
     }
 }
